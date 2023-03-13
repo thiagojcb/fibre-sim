a95c03f73b92491c4795b7ab943b8b8d03d8a789
Thiago Bezerra <thiagojcb@gmail.com>
2023-03-13 22:59:44 +0000

    typo fix

--- Att_lenght.cpp
+++ Att_lenght.cpp
@@ -1,4 +1,4 @@
-//macro to draw the attenuation lenght of the fibre (2 expo)
+//macro to draw the attenuation length of the fibre (2 expo)
 
 {
   TF1 f1("f1","[0]*exp(-x/[1])+[2]*exp(-x/[3])",0,30);
@@ -11,13 +11,13 @@
   f2.SetParameters(20,30);
   f2.SetLineColor(kBlue);
   f2.SetLineWidth(3);
-  f2.SetTitle("Short Att. Lenght, 30 cm");
+  f2.SetTitle("Short Att. Length, 30 cm");
 
   TF1 f3("f3","[0]*exp(-x/[1])",0,30);
   f3.SetParameters(10,500);
   f3.SetLineColor(kRed);
   f3.SetLineWidth(3);
-  f3.SetTitle("Long Att. Lenght, 500 cm");
+  f3.SetTitle("Long Att. Length, 500 cm");
 
   f1.Draw();
   f1.GetXaxis()->SetTitle("Distance (cm)");
