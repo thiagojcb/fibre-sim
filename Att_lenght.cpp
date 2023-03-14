abe978eaaa3fdec38280123eab86f5fad3a2574c
Thiago Bezerra <thiagojcb@gmail.com>
2023-03-14 11:51:27 +0000

    assuming longer fibre. printing info

--- Att_lenght.cpp
+++ Att_lenght.cpp
@@ -1,19 +1,19 @@
 //macro to draw the attenuation length of the fibre (2 expo)
 
 {
-  TF1 f1("f1","[0]*exp(-x/[1])+[2]*exp(-x/[3])",0,30);
+  TF1 f1("f1","[0]*exp(-x/[1])+[2]*exp(-x/[3])",0,100);
   f1.SetParameters(20,30,10,500);
   f1.SetLineColor(kBlack);
   f1.SetLineWidth(3);
   f1.SetTitle("Total");
 
-  TF1 f2("f2","[0]*exp(-x/[1])",0,30);
+  TF1 f2("f2","[0]*exp(-x/[1])",0,100);
   f2.SetParameters(20,30);
   f2.SetLineColor(kBlue);
   f2.SetLineWidth(3);
   f2.SetTitle("Short Att. Length, 30 cm");
 
-  TF1 f3("f3","[0]*exp(-x/[1])",0,30);
+  TF1 f3("f3","[0]*exp(-x/[1])",0,100);
   f3.SetParameters(10,500);
   f3.SetLineColor(kRed);
   f3.SetLineWidth(3);
@@ -33,6 +33,15 @@
   gPad->SetGridx();
   gPad->SetTicky();
   gPad->SetTickx();
-  
 
+  cout<<"Total light at 65cm: "<< f1.Eval(65) <<endl;
+  cout<<"Short light component at 65cm: "<< f2.Eval(65) <<endl;
+  cout<<"Long light component at 65cm: "<< f3.Eval(65) <<endl;
+
+  TLine l1(65,0,65,35);
+  l1.SetLineWidth(3);
+  l1.SetLineStyle(7);
+  l1.Draw();
+    
+  
 }
