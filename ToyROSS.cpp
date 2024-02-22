c8e24a3f2530ba45b9a8f33028278a3a9ea57a01
Thiago Bezerra <thiagojcb@gmail.com>
2024-02-22 13:15:14 +0000

    adding more comments

--- ToyROSS.cpp
+++ ToyROSS.cpp
@@ -18,6 +18,7 @@
   Double_t Att_leng  = 5.;                          // fibre attenuation length, in metres
   Double_t factor    = 3.41;                        // factor to boost or decrease LY of Susie sim
   Double_t myEff     = 100*(factor*0.108*0.4*0.9);  // trapping, QE, coupling
+                                                    // with all these numbers + Susie 2 MeV electron file: 400 PE/MeV
 
   // loading input file
   TFile *fMyFile = new TFile("electron_2MeV.root");
